a1c1cfe5c579b60c5e1c2164c3f38280a03eaf65
Zehavi-Klain <z6773503@gmail.com>
2025-05-07 01:06:32 +0300

    change

--- BehaviorAnalystProject/BehaviorAnalystProject/Program.cs
+++ BehaviorAnalystProject/BehaviorAnalystProject/Program.cs
@@ -1,11 +1,57 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using Common.Dto;
+using Service.Interfaces;
+using Repository.Interfaces;
+using Repository.Entities;
+using Repository.Repositories;
+using Service.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Mock;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+// רישום שירותים
+builder.Services.AddScoped<IService<AnalystDto>, AnalystService>();
+builder.Services.AddScoped<IRepository<Analyst>, AnalystRepository>(); // נוספה שורה זו
+builder.Services.AddDbContext<DataBase>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  // ודא שהחיבור למסד נתונים מוגדר ב-appsettings.json
+builder.Services.AddScoped<IContext, DataBase>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -13,13 +59,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
---
+++ BehaviorAnalystProject/Service/Services/MyMapper.cs
@@ -0,0 +1,43 @@
+﻿using Common.Dto;
+using Repository.Entities;
+using System;
+using System.IO;
+using AutoMapper;
+
+namespace Service.Services
+{
+    internal class MyMapper
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Forms/");
+        private readonly IMapper _mapper;
+
+        public MyMapper()
+        {
+            // יצירת קונפיגורציה של AutoMapper
+            var config = new MapperConfiguration(cfg =>
+            {
+                // form
+                cfg.CreateMap<Form, FormDto>()
+                    .ForMember(dest => dest.ArrFile, opt => opt.MapFrom(src => File.ReadAllBytes(path + src.FileUrl)));
+                cfg.CreateMap<FormDto, Form>()
+                    .ForMember(dest => dest.FileUrl, opt => opt.MapFrom(src => src.fileImage.FileName));
+
+                // analyst
+                cfg.CreateMap<AnalystDto, Analyst>();
+                cfg.CreateMap<Analyst, AnalystDto>();
+
+                // child
+                cfg.CreateMap<Child, ChildDto>().ReverseMap();
+            });
+
+            // יצירת Mapper מתוך הקונפיגורציה
+            _mapper = config.CreateMapper();
+        }
+
+        // דוגמה לשימוש ב-Mapper
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return _mapper.Map<TSource, TDestination>(source);
+        }
+    }
+}
