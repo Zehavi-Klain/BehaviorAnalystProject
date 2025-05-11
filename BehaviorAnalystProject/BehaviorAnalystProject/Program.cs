dd2b9579f323789035e23a1a50e0e4e9c5579995
Naomi-Samai-0 <work597983@gmail.com>
2025-05-11 03:14:19 +0300

    i dont know

--- BehaviorAnalystProject/BehaviorAnalystProject/Program.cs
+++ BehaviorAnalystProject/BehaviorAnalystProject/Program.cs
@@ -1,11 +1,30 @@
+using Common.Dto;
+using Service.Interfaces;
+using Repository.Interfaces;
+using Repository.Entities;
+using Repository.Repositories;
+using Service.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Mock;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+// רישום שירותים
+builder.Services.AddScoped<IService<AnalystDto>, AnalystService>();
+builder.Services.AddScoped<IRepository<Analyst>, AnalystRepository>(); // נוספה שורה זו
+builder.Services.AddDbContext<DataBase>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  // ודא שהחיבור למסד נתונים מוגדר ב-appsettings.json
+builder.Services.AddScoped<IContext, DataBase>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -13,11 +32,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
